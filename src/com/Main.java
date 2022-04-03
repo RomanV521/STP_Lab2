e068cc7e399c97c9d38972158181fe93af02877f
RomanV521 <romav5166@gmail.com>
2022-04-03 16:43:32 +0300

    Completed 48

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,6 +1,7 @@
 package com;
 
 
+import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -38,8 +39,48 @@ public class Main {
         }
     }
 
-
+    /**
+     * Задана последовательность N вещественных чисел. Вычислить сумму чисел, порядковые номера которых являются:
+     * а) простыми числами;
+     * б) числами Фибоначчи.
+     */
     private static void task_48(){
+        System.out.println("Task 48:\nЗадана последовательность N вещественных чисел. Вычислить сумму чисел, порядковые номера которых являются:\n\tа) простыми числами;\n\tб) числами Фибоначчи.\n");
+
+        int n=random.nextInt(45)+15, j=0;
+        double[] arrays=new double[n];
+        double d,sumF=0, sumPN=0;
+
+        System.out.println("Количество N: "+n+"\n\nPN  |Fib.|\nSum:|Sum:|Значения:");
+
+
+        for (int i=0; i<n; i++){
+            arrays[i]= Math.round((random.nextDouble(14)+3)*100.0)/100.0;// (3..17), ограничение до сотых
+
+            //A)
+            if(i<=1 || (i+1)%2!=0 && (i+1)%3!=0 && (i+1)%5!=0){
+                System.out.print("\'+\' ");
+                sumPN+=arrays[i];
+            }
+
+            else {
+                System.out.print(" \t");
+            }
+
+            //Б) Fibonacci
+            if(Math.sqrt(5*(i+1)*(i+1)-4) - (int)Math.sqrt(5*(i+1)*(i+1)-4) == 0 || Math.sqrt(5*(i+1)*(i+1)+4) - (int)Math.sqrt(5*(i+1)*(i+1)+4) == 0){
+                System.out.print("|\'+\' | ");
+                sumF += arrays[i];
+            }
+            else {
+                System.out.print("|\t | ");
+            }
+            System.out.println(" "+(i+1)+" = "+arrays[i]);
+        }
+
+        System.out.println("\nA) Сумму чисел, порядковые номера которых являются простыми числами: "+ String.format("%.2f",sumPN));
+        //Числа Фибоначчи: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144...
+        System.out.println("Б) Сумму чисел, порядковые номера которых являются числами Фибоначчи: " + String.format("%.2f",sumF+arrays[0]));
 
     }
 
